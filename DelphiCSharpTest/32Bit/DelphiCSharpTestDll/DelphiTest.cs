835994ee13e6466e5fef7b33329a89128c47e1b3
MerlinEl (Orien) <merlin_el@hotmail.com>
2022-02-10 10:35:08 +0100

    up

---
+++ DelphiCSharpTest/32Bit/DelphiCSharpTestDll/DelphiTest.cs
@@ -0,0 +1,93 @@
+﻿using RGiesecke.DllExport;
+using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace DelphiCSharpTestDll {
+    // Interface
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IDelphiTest {
+        void SayHello(string msg);
+        TMyRec ModifyRecord(TMyRec rec);
+    }
+    // Class
+    public class DelphiTest : IDelphiTest {
+
+        [DllExport("TestVal", CallingConvention = CallingConvention.StdCall)]
+        public static TMyRec TestVal(TMyRec rec) {
+
+            //rec.str1 = Marshal.StringToHGlobalUni(Marshal.PtrToStringUni(rec.str1) + "a");
+            //rec.str2 = Marshal.StringToHGlobalUni(Marshal.PtrToStringUni(rec.str1) + "b");
+            MessageBox.Show("str1" + rec.str1);
+            rec.str1 += "a";
+            rec.str2 += "b";
+            rec.int1 += 10;
+            rec.int2 += 20;
+            rec.dbl1 += 0.5;
+            rec.dbl2 += 1.2;
+            rec.bool1 = !rec.bool1;
+            rec.bool2 = !rec.bool2;
+            return rec;
+        }
+
+        [DllExport("TestValRef", CallingConvention = CallingConvention.StdCall)]
+        public static bool TestValRef(ref TMyRec rec) {
+
+            //rec.str1 = Marshal.StringToHGlobalUni(Marshal.PtrToStringUni(rec.str1) + "a");
+            //rec.str2 = Marshal.StringToHGlobalUni(Marshal.PtrToStringUni(rec.str1) + "b");
+            rec.str1 += "a";
+            rec.str2 += "b";
+            rec.int1 += 10;
+            rec.int2 += 20;
+            rec.dbl1 += 0.5;
+            rec.dbl2 += 1.2;
+            rec.bool1 = !rec.bool1;
+            rec.bool2 = !rec.bool2;
+            return true;
+        }
+
+        //public static TMyRec TestStr(TMyRec rec) {
+
+        //    rec.str1 = Marshal.StringToHGlobalUni(Marshal.PtrToStringUni(rec.str1) + "a");
+        //    rec.str2 = Marshal.StringToHGlobalUni(Marshal.PtrToStringUni(rec.str1) + "b");
+        //    return rec;
+        //}
+
+        [DllExport("Plus", CallingConvention = CallingConvention.StdCall)]
+        public static int Plus(int n) {
+
+            return n + 10;
+        }
+
+        [DllExport("GetInstance", CallingConvention = CallingConvention.StdCall)]
+        public static bool GetInstance(ref DelphiTest x) {
+
+            x = new DelphiTest(); // create self instance
+            return true;
+        }
+
+        public void SayHello(string msg) => MessageBox.Show("CSharp say:" + msg);
+
+        public TMyRec ModifyRecord(TMyRec rec) {
+            
+            rec.int1 += 10;
+            rec.int2 += 20;
+            return rec;
+        }
+    }
+}
+
+/*
+
+            Form form = new Form1Dll();
+            form.Show();
+            form.Log(" Delphi Record: ");
+            form.Log("\tstr1:" + rec.str1);
+            form.Log("\tstr2:" + rec.str2);
+            form.Log("\tint1:" + rec.int1);
+            form.Log("\tint2:" + rec.int2);
+            form.Log("\tdbl1:" + rec.dbl1);
+            form.Log("\tdbl2:" + rec.dbl2);
+            form.Log("\tbool1:" + rec.bool1);
+            form.Log("\tbool2:" + rec.bool1);
+ */
---
+++ DelphiCSharpTest/32Bit/DelphiCsharpTest/Form1.cs
@@ -0,0 +1,93 @@
+﻿using DelphiCSharpTestDll;
+using System;
+using System.Windows.Forms;
+
+namespace DelphiCsharpTest {
+    public partial class Form1 : Form {
+
+        public Form1() {
+            InitializeComponent();
+            TestSize();
+            //TestVarsStatic();
+            //TestVarsInstance();
+        }
+
+        private void TestSize() {
+            //    var r = typeof(TMyRec);
+            //    Log("rec: " + Marshal.SizeOf(r).ToString());
+            Log("bool size: " + sizeof(bool).ToString());
+        }
+
+        private void TestVarsInstance() {
+            
+            DelphiTest inst = new DelphiTest();
+            DelphiTest.GetInstance(ref inst);
+            //inst.SayHello("Rene");
+
+            var rec = new TMyRec {
+                int1 = 1,
+                int2 = 2
+            };
+
+            Log("Instance Delphi >" + Environment.NewLine + "\tRecord Send: ");
+            Log("\t\tint1:" + rec.int1);
+            Log("\t\tint2:" + rec.int2);
+
+            var rec2 = inst.ModifyRecord(rec);
+
+            Log("\tRecord Get: ");
+            Log("\t\tint1:" + rec2.int1);
+            Log("\t\tint2:" + rec2.int2);
+        }
+
+        private void Log(string str) => Label1.AppendText(Environment.NewLine + str);
+
+        private void TestVarsStatic() {
+
+            var rec = new TMyRec {
+                int1 = 1,
+                int2 = 2
+            };
+
+            Log("Static Delphi  >"+Environment.NewLine+"\tRecord Send: ");
+            Log("\tint1:" + rec.int1);
+            Log("\tint2:" + rec.int2);
+
+            var rec2 = DelphiTest.TestVal(rec);
+
+            Log("\tRecord Get: ");
+            Log("\tint1:" + rec2.int1);
+            Log("\tint2:" + rec2.int2);
+        }
+
+        //private unsafe void TestVars() {
+        //    var r = typeof(TMyRec);
+        //    Log("rec: " + Marshal.SizeOf(r).ToString());
+        //    Log("rec: " + sizeof(TMyRec).ToString());
+        //}
+
+        //[DllExport]
+        //public static TMyRec TestVal(TMyRec rec) {
+
+        //    Log(" Delphi Record: ");
+        //    Log("\tstr1:" + rec.str1);
+        //    Log("\tstr2:" + rec.str2);
+        //    Log("\tint1:" + rec.int1);
+        //    Log("\tint2:" + rec.int2);
+        //    Log("\tdbl1:" + rec.dbl1);
+        //    Log("\tdbl2:" + rec.dbl2);
+        //    Log("\tbool1:" + rec.bool1);
+        //    Log("\tbool2:" + rec.bool1);
+
+        //    rec.str1 = Marshal.StringToHGlobalUni(Marshal.PtrToStringUni(rec.str1) + "a");
+        //    rec.str2 = Marshal.StringToHGlobalUni(Marshal.PtrToStringUni(rec.str1) + "b");
+        //    rec.int1 += 10;
+        //    rec.int2 += 20;
+        //    rec.dbl1 += 0.5;
+        //    rec.dbl2 += 1.2;
+        //    rec.bool1 = !rec.bool1;
+        //    rec.bool2 = !rec.bool2;
+        //    return rec;
+        //}
+    }
+}
