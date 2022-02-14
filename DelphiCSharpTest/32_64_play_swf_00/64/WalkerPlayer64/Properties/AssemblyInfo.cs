7a26ac985f2579703eaddb13f9fb33dc34532db1
MerlinEl (Orien) <merlin_el@hotmail.com>
2022-02-14 12:14:39 +0100

    up

--- DelphiCSharpTest/32_64_play_swf_00/64/WalkerPlayer64/Properties/AssemblyInfo.cs
+++ DelphiCSharpTest/32_64_play_swf_00/64/WalkerPlayer64/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("24291fe6-859d-45a9-90d4-5a866e19725e")]
---
+++ DelphiCSharpTest/32_64_play_swf_00/64/bk/_extract_ocx_dll.bat
@@ -0,0 +1,2 @@
+aximp.exe -source  Flash64_14_0_0_176.ocx
+pause
