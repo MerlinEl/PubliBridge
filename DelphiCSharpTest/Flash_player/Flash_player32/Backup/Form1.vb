40255142c4b1e144abc55a886c451d995d54f756
MerlinEl (Orien) <merlin_el@hotmail.com>
2022-02-16 17:33:34 +0100

    add

---
+++ DelphiCSharpTest/Flash_player/Flash_player32/Backup/Form1.vb
@@ -0,0 +1,138 @@
+Public Class Form1
+    Inherits System.Windows.Forms.Form
+
+#Region " Windows Form Designer generated code "
+
+    Public Sub New()
+        MyBase.New()
+
+        'This call is required by the Windows Form Designer.
+        InitializeComponent()
+
+        'Add any initialization after the InitializeComponent() call
+
+    End Sub
+
+    'Form overrides dispose to clean up the component list.
+    Protected Overloads Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing Then
+            If Not (components Is Nothing) Then
+                components.Dispose()
+            End If
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents AxShockwaveFlash1 As AxShockwaveFlashObjects.AxShockwaveFlash
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    <System.Diagnostics.DebuggerStepThrough()> Private Sub InitializeComponent()
+        Dim resources As System.Resources.ResourceManager = New System.Resources.ResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog
+        Me.AxShockwaveFlash1 = New AxShockwaveFlashObjects.AxShockwaveFlash
+        Me.Button2 = New System.Windows.Forms.Button
+        Me.Button3 = New System.Windows.Forms.Button
+        Me.Button4 = New System.Windows.Forms.Button
+        CType(Me.AxShockwaveFlash1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(112, 320)
+        Me.Button1.Name = "Button1"
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        '
+        'AxShockwaveFlash1
+        '
+        Me.AxShockwaveFlash1.Enabled = True
+        Me.AxShockwaveFlash1.Location = New System.Drawing.Point(16, 8)
+        Me.AxShockwaveFlash1.Name = "AxShockwaveFlash1"
+        Me.AxShockwaveFlash1.OcxState = CType(resources.GetObject("AxShockwaveFlash1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxShockwaveFlash1.Size = New System.Drawing.Size(576, 296)
+        Me.AxShockwaveFlash1.TabIndex = 1
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(16, 320)
+        Me.Button2.Name = "Button2"
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Button2"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(208, 320)
+        Me.Button3.Name = "Button3"
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Button3"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(304, 320)
+        Me.Button4.Name = "Button4"
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "Button4"
+        '
+        'Form1
+        '
+        Me.AutoScaleBaseSize = New System.Drawing.Size(5, 13)
+        Me.ClientSize = New System.Drawing.Size(608, 366)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.AxShockwaveFlash1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.AxShockwaveFlash1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+#End Region
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If Button1.Text = "Stop" Then
+            Button1.Text = "Play"
+        ElseIf Button1.Text = "Play" Then
+            Button1.Text = "Stop"
+        End If
+        AxShockwaveFlash1.Play()
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Dim open As New OpenFileDialog
+        open = New OpenFileDialog
+        open.Filter = "Image Files(*.swf)|*.swf"
+        If (open.ShowDialog() = DialogResult.OK) Then
+            AxShockwaveFlash1.Movie = open.FileName
+        End If
+
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        AxShockwaveFlash1.Rewind()
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        AxShockwaveFlash1.Forward()
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Button2.Text = "Load"
+        Button1.Text = "Stop"
+        Button3.Text = "Rewind"
+        Button4.Text = "Forward"
+    End Sub
+End Class
